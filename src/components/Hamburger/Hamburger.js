ea3dfe7befd7f2097e235c95fc992927dd2d0fd6
Aleksander Gasior <aleksandergasior@MacBook-Air-Aleksander.local>
2021-11-28 15:14:51 +0100

    refactor

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import classNames from 'classnames';
-import './Hamburger.scss';
+import React from "react";
+import classNames from "classnames";
+import "./Hamburger.scss";
 
 const Hamburger = ({ toggleMenu, isOpen }) => {
-  const hamburgerClasses = classNames('hamburger', {
-    'hamburger--active': isOpen,
+  const hamburgerClasses = classNames("hamburger", {
+    "hamburger--active": isOpen,
   });
   return (
-    <button onClick={toggleMenu} className={hamburgerClasses}>
+    <button onClick={toggleMenu} className={hamburgerClasses} alt="menu-icon">
       <span className="hamburger__bar" />
       <span className="hamburger__bar" />
       <span className="hamburger__bar" />
