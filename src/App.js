a62d9f5f5b6b4878a2acf4f0c54b4f7287461692
Aleksander Gasior <aleksandergasior@MacBook-Air-Aleksander.local>
2021-11-28 14:40:30 +0100

    add lazy to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, Suspense, lazy } from "react";
 import { ContextReservation } from "./context/Context.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./hooks/ScrollToTop";
 import Header from "./containers/Header/Header";
 import Footer from "./containers/Footer/Footer";
 import Homepage from "./pages/Homepage/Homepage";
-import MenuPage from "./pages/MenuPage/MenuPage";
-import ServicesPage from "./pages/ServicesPage/ServicesPage";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import ContactPage from "./pages/ContactPage/ContactPage";
+// import MenuPage from "./pages/MenuPage/MenuPage";
+// import ServicesPage from "./pages/ServicesPage/ServicesPage";
+// import AboutPage from "./pages/AboutPage/AboutPage";
+// import ContactPage from "./pages/ContactPage/ContactPage";
 import { reservation } from "./content/reservation";
 import "./styles/global.scss";
+
+const MenuPage = lazy(() => import("./pages/MenuPage/MenuPage"));
+const ServicesPage = lazy(() => import("./pages/ServicesPage/ServicesPage"));
+const AboutPage = lazy(() => import("./pages/AboutPage/AboutPage"));
+const ContactPage = lazy(() => import("./pages/ContactPage/ContactPage"));
 
 const App = () => {
   const [reservationContext, setReservationContext] = useState(reservation);
@@ -24,10 +29,18 @@ const App = () => {
           <Header />
           <Routes>
             <Route exact path="/coffee-blend/" element={<Homepage />} />
-            <Route path="/coffee-blend/menu" element={<MenuPage />} />
-            <Route path="/coffee-blend/services" element={<ServicesPage />} />
-            <Route path="/coffee-blend/about" element={<AboutPage />} />
-            <Route path="/coffee-blend/contact" element={<ContactPage />} />
+            <Suspense fallback={<div />}>
+              <Route path="/coffee-blend/menu" element={<MenuPage />} />
+            </Suspense>
+            <Suspense fallback={<div />}>
+              <Route path="/coffee-blend/services" element={<ServicesPage />} />
+            </Suspense>
+            <Suspense fallback={<div />}>
+              <Route path="/coffee-blend/about" element={<AboutPage />} />
+            </Suspense>
+            <Suspense fallback={<div />}>
+              <Route path="/coffee-blend/contact" element={<ContactPage />} />
+            </Suspense>
           </Routes>
           <Footer />
         </ContextReservation.Provider>
