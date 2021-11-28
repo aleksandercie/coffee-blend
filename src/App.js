067a11c80ed1c3f1e23227d14c251d5f8deb7213
Aleksander Gasior <aleksandergasior@MacBook-Air-Aleksander.local>
2021-11-28 11:42:20 +0100

    remove suspense from app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, Suspense, lazy } from "react";
+import React, { useState } from "react";
 import { ContextReservation } from "./context/Context.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./hooks/ScrollToTop";
 import Header from "./containers/Header/Header";
+import Footer from "./containers/Footer/Footer";
 import Homepage from "./pages/Homepage/Homepage";
 import MenuPage from "./pages/MenuPage/MenuPage";
 import ServicesPage from "./pages/ServicesPage/ServicesPage";
@@ -10,8 +11,6 @@ import AboutPage from "./pages/AboutPage/AboutPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import { reservation } from "./content/reservation";
 import "./styles/global.scss";
-
-const Footer = lazy(() => import("./containers/Footer/Footer"));
 
 const App = () => {
   const [reservationContext, setReservationContext] = useState(reservation);
@@ -30,9 +29,7 @@ const App = () => {
             <Route path="/coffee-blend/about" element={<AboutPage />} />
             <Route path="/coffee-blend/contact" element={<ContactPage />} />
           </Routes>
-          <Suspense fallback={<div />}>
-            <Footer />
-          </Suspense>
+          <Footer />
         </ContextReservation.Provider>
       </>
     </BrowserRouter>
