39f3f84eb61a288598ccd7f52b39f5f6c7ce1d17
Aleksander Gasior <aleksandergasior@MacBook-Air-Aleksander.local>
2021-11-29 16:06:55 +0100

    lazy form

--- src/containers/Intro/Intro.js
+++ src/containers/Intro/Intro.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import Container from '../../common/Container/Container';
-import Details from '../../components/Details/Details';
-import Form from '../../components/Form/Form';
-import FadeIn from '../../components/FadeIn/FadeIn';
-import './Intro.scss';
+import React, { Suspense, lazy } from "react";
+import Container from "../../common/Container/Container";
+import Details from "../../components/Details/Details";
+import FadeIn from "../../components/FadeIn/FadeIn";
+import "./Intro.scss";
+
+const Form = lazy(() => import("../../components/Form/Form"));
 
 const Intro = () => (
   <Container customClass="intro" variant>
     <FadeIn option="fade-right">
       <Details />
     </FadeIn>
-    <Form btnVariant="appointment" />
+    <Suspense fallback={<div />}>
+      <Form btnVariant="appointment" />
+    </Suspense>
   </Container>
 );
 
